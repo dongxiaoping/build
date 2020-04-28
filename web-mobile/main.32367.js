9567fa60ee6c6f8d1d5e831feeecae5f868c61ef
dxp <dongxiaoping>
2020-04-28 15:50:46 +0800

    添加客服

--- web-mobile/main.32367.js
+++ web-mobile/main.32367.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8dfa0.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.4e337.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,12 +178,12 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.4642a.js');
+        require('src/settings.5836e.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.4642a.js');
+        require('src/settings.5836e.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
