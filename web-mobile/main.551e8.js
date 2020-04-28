24b4f61e8ee1ad5bb85af1027f307b3760fdde81
dxp <dongxiaoping>
2020-04-28 16:03:08 +0800

    提交

--- web-mobile/main.551e8.js
+++ web-mobile/main.551e8.js
@@ -178,12 +178,12 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.5836e.js');
+        require('src/settings.20345.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.5836e.js');
+        require('src/settings.20345.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
