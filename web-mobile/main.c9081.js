69c6998e6d41bb3d95c6e49ae0582e1eeef25fba
dxp <dongxiaoping>
2020-05-04 20:41:42 +0800

    aa

--- web-mobile/main.c9081.js
+++ web-mobile/main.c9081.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.260ce.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.448c9.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
