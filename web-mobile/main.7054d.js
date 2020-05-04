3946cf231d64ab7774f4ca3c2dd025f983d3ebb5
dxp <dongxiaoping>
2020-05-04 10:20:13 +0800

    aa

--- web-mobile/main.7054d.js
+++ web-mobile/main.7054d.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.d80cd.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.79bbe.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
