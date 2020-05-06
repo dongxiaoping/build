9150256ab8187376c1dd0340c0c5ca8d86f0cef1
dxp <dongxiaoping>
2020-05-06 21:06:17 +0800

    a

--- web-mobile/main.d0118.js
+++ web-mobile/main.d0118.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.82930.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.2863c.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
