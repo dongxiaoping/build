018e20e5b7b3f5e4dfead1142683fce2c6980327
dxp <dongxiaoping>
2020-05-11 21:01:52 +0800

    aa

--- web-mobile/main.06461.js
+++ web-mobile/main.06461.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.56d7e.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.474e2.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,12 +178,12 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.90c9a.js');
+        require('src/settings.55552.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.90c9a.js');
+        require('src/settings.55552.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
