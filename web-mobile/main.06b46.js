a3e3e8d4d113580449b90ac224b10577359aef4c
dxp <dongxiaoping>
2020-09-30 21:39:26 +0800

    修改版本号

--- web-mobile/main.06b46.js
+++ web-mobile/main.06b46.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.b3d12.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.d7d85.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
