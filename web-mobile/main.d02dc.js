e97d3336dcd4d3b8a02d28f730f335cbf914761d
dxp <dongxiaoping>
2020-10-10 18:08:19 +0800

    更新

--- web-mobile/main.d02dc.js
+++ web-mobile/main.d02dc.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.d7d85.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.a4dc5.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,12 +178,12 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.3983c.js');
+        require('src/settings.c0bb2.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.3983c.js');
+        require('src/settings.c0bb2.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
