ab0ae62c84afeae988ee1e5bab4f22a7d51e12e5
dxp <dongxiaoping>
2020-10-21 18:01:12 +0800

    更新

--- web-mobile/main.5234c.js
+++ web-mobile/main.5234c.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.a4dc5.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8c7fe.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
