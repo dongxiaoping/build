66993d3c3fe7f26a69f04bd738ea1b837ccceda8
Administrator <466476120@qq.com>
2020-12-11 17:51:22 +0800

    21

--- web-mobile/main.5c261.js
+++ web-mobile/main.5c261.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.da7c4.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8a502.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
