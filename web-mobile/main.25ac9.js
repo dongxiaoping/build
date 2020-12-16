da659e8633f369bb912af25ba1f04f1535c1963e
Administrator <466476120@qq.com>
2020-12-16 15:14:04 +0800

    23

--- web-mobile/main.25ac9.js
+++ web-mobile/main.25ac9.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.7a766.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.edac4.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
