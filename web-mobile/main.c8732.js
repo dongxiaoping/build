707db9110a411b72482d4853d2b3e7c201eb33cb
dxp <dongxiaoping>
2020-12-31 19:58:05 +0800

    update

--- web-mobile/main.c8732.js
+++ web-mobile/main.c8732.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.6d804.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.0a235.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,12 +178,12 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.e3ee0.js');
+        require('src/settings.21e86.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.e3ee0.js');
+        require('src/settings.21e86.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
