8f0ea0130a57e5760c03df2a100569359b691377
Administrator <466476120@qq.com>
2021-02-07 21:57:12 +0800

    fix

--- web-mobile/main.a341d.js
+++ web-mobile/main.a341d.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.df8c3.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.5e8da.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
